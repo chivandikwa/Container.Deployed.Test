5144e22e6ca7dfb4a4c42c68b1e3f5dca562e063
Thulani Chivandikwa <t.chivandikwa@cardano.com>
2019-12-07 15:29:35 +0100

    Create base project

---
+++ Container.Deployed.Test/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Container.Deployed.Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            // Fetch a variable that should be resolved by env key
+            return Environment.GetEnvironmentVariable("DBCONNECTIONSTRING");
+        }
+    }
+}
---
+++ Container.Deployed.Test/Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Container.Deployed.Test
+{
+    using EnvKey;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            var envKey = new EnvKeyConfig(); 
+            envKey.Load();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
